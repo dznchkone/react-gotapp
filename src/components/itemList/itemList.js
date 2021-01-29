1dfcaff005686b67ae49ee7e8db1c738e1ca9b95
Igor Vavilin <igorvv.vn@gmail.com>
2021-01-29 14:49:21 +0300

    103 Компонент высшего порядка (HOC)

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,54 +1,10 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-
-
 import {Card, ListGroup, ListGroupItem, Spinner} from 'reactstrap';
 import ErrorMessage from "../errorMessage";
+import gotService from "../../services/gotService";
 
 
-export default class ItemList extends Component {
-
-    state = {
-        itemList: null,
-        error: false
-    }
-
-    static defaultProps = {
-        onItemSelected: () =>{}
-    }
-
-    static propTypes = {
-        onItemSelected: PropTypes.func
-    }
-
-    componentDidMount() {
-        const {getData} = this.props;
-
-        getData()
-            .then((itemList)=>{
-                this.setState(
-                    {
-                        itemList
-                    }
-                )
-            })
-            .catch(this.onError)
-    }
-
-    componentDidCatch() {
-        this.setState({
-            itemList: null,
-            error: true
-        })
-    }
-
-    onError = () =>{
-        this.setState({
-            itemList: null,
-            error: true
-        })
-    }
-
+class ItemList extends Component {
     renderItems(arr){
         return arr.map((item)=>{
             const {id} = item;
@@ -66,20 +22,12 @@ export default class ItemList extends Component {
     }
 
     render() {
-        const {itemList, error} = this.state
-
-        let temp = null;
-        if(!itemList&&error) {
-            temp = <ErrorMessage message="Whoops...Can't loading characters list"/>
-        } else if(!itemList) {
-            temp =  <Spinner> </Spinner>
-        }
-        const items = !itemList ? null : this.renderItems(itemList);
+        const {data} = this.props
+        const items = this.renderItems(data);
 
         return (
             <Card body className="p-3 rounded">
                 <ListGroup>
-                    {temp}
                     {items}
                 </ListGroup>
             </Card>
@@ -90,3 +38,53 @@ export default class ItemList extends Component {
 ItemList.defaultProps = {
     onItemSelected: () =>{}
 }
+
+const withData = (View, getData)=>{
+    return class extends Component {
+        state = {
+            data: null,
+            error: false
+        }
+
+        componentDidMount() {
+            getData()
+                .then((data)=>{
+                    this.setState(
+                        {
+                            data
+                        }
+                    )
+                })
+                .catch(this.onError)
+        }
+
+        componentDidCatch() {
+            this.setState({
+                data: null,
+                error: true
+            })
+        }
+
+        onError = () =>{
+            this.setState({
+                data: null,
+                error: true
+            })
+        }
+
+        render() {
+            const {data, error} = this.state
+
+            if(!data&&error) {
+                return <ErrorMessage message="Whoops...Can't loading characters list"/>
+            } else if(!data) {
+                return  <Spinner> </Spinner>
+            }
+
+            return <View {...this.props} data={data}/>
+        }
+    }
+}
+
+const {getAllCharacters} = new gotService();
+export default withData(ItemList, getAllCharacters);
